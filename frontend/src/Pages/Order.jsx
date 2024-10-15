5e4541f989c3e0ac51f22c85c30486b16523f475
KalapacsJoco <kalapacs.joco79@gmail.com>
2024-10-15 20:29:54 +0300

    order frissites utani hiba kijavitva

--- frontend/src/Pages/Order.jsx
+++ frontend/src/Pages/Order.jsx
@@ -2,16 +2,14 @@ import { useContext } from "react";
 import { AppContext } from "../Context/AppContext";
 
 function Order() {
-  const { cart } = useContext(AppContext);
-  // const [qty, setQty] = useState({cart.qty})
-  const { user } = useContext(AppContext);
+  const { cart, user } = useContext(AppContext); // Hozzáférünk a user és cart objektumokhoz
 
   return (
     <>
-    <section className="text-gray-100"> 
-      <article className="flex flex-col text-gray-100 w-50">
-        <h1>Rendelés</h1>
-          <div className="">
+      <section className="text-gray-100"> 
+        <article className="flex flex-col text-gray-100 w-50">
+          <h1>Rendelés</h1>
+          <div>
             <table>
               <thead>
                 <tr>
@@ -24,28 +22,34 @@ function Order() {
               </thead>
               <tbody>
                 {Object.entries(cart).map(([key, item], index) => (
-                  <tr key={key}> {/* Itt használjuk a key attribútumot */}
+                  <tr key={key}>
                     <td>{index + 1}</td> {/* Sorszám */}
                     <td>{item.name}</td> {/* Étel neve */}
-                    <td>{item.qty}</td> {/* Étel neve */}
-                    {/* <input type="number" > </input> */}
+                    <td>{item.qty}</td> {/* Mennyiség */}
                     <td>{item.price} Ft</td> {/* Ár */}
-                    <td>{item.price * item.qty}  Ft</td> 
+                    <td>{item.price * item.qty} Ft</td> {/* Összesen */}
                   </tr>
                 ))}
               </tbody>
             </table>
-        </div>
-      </article>
-      <article>
-        <ul> {user.first_name}</ul>
-        <ul> {user.last_name}</ul>
-        <ul> {user.email}</ul>
-        <ul> {user.phone}</ul>
-        <ul> {user.street}</ul>
-        <ul> {user.street_number}</ul>
+          </div>
+        </article>
 
-      </article>
+        <article>
+          {/* Ellenőrizzük, hogy a user létezik-e, mielőtt megjelenítjük az adatait */}
+          {user ? (
+            <>
+              <ul>{user.first_name}</ul>
+              <ul>{user.last_name}</ul>
+              <ul>{user.email}</ul>
+              <ul>{user.phone}</ul>
+              <ul>{user.street}</ul>
+              <ul>{user.street_number}</ul>
+            </>
+          ) : (
+            <p>Nincs felhasználói adat.</p> // Hiba esetén vagy ha a user nem létezik
+          )}
+        </article>
       </section>
     </>
   );
